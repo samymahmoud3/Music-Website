27baa9526de85cfd624c032a0d3765b90c127ba0
wowtrika22 <samymahmoud2532001@gmail.com>
2023-05-01 22:05:26 +0300

    add posters&songNames

--- js/custom.js
+++ js/custom.js
@@ -131,24 +131,6 @@ Array.from(document.getElementsByClassName('songItem')).forEach((e, i) => {
 
 
 
-//play&pause masterPlay music
-let masterPlay = document.getElementById('masterPlay');
-let wave = document.getElementById('wave');
-
-masterPlay.addEventListener('click', () =>{
-    if(music.paused || music.currentTime <= 0){
-        music.play();
-        wave.classList.add('active1');
-        masterPlay.classList.remove('bi-play-fill');
-        masterPlay.classList.add('bi-pause-fill');
-    } else {
-        music.pause();
-        wave.classList.remove('active1');
-        masterPlay.classList.add('bi-play-fill');
-        masterPlay.classList.remove('bi-pause-fill');
-    }
-});
-
 
 //============scroll popular-songs============================
 let pop_song_right = document.getElementById('pop_song_right');
